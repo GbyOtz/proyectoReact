8eb91441a20c5006c11c7b996250ab78210e7de0
GbyOtz <anagabrielaortizv16@gmail.com>
2023-10-19 08:52:48 -0400

    select

--- prueba/src/App.js
+++ prueba/src/App.js
@@ -2,12 +2,14 @@
 import './componentes/Header/Header.js';
 import './App.css';
 import Header from './componentes/Header/Header.js';
+import Formulario from './componentes/Formulario/Formulario.js';
 
 function App() {
   return ( 
        
     <div>
       <Header/>
+      <Formulario/>
     </div>
   );
 }
--- prueba/src/componentes/Formulario/Formulario.js
+++ prueba/src/componentes/Formulario/Formulario.js
@@ -1,11 +1,15 @@
 import "./Formulario.css"
-import Texto from "../../Texto/Texto.js"
+import Texto from "../Texto/Texto.js"
 
 const Formulario=()=>{
     return <section className="formulario">
         <form>
             <h2>RELLENA EL FORMULARIO PARA CREAR UN NUEVO PERSONAJE</h2>
             <Texto titulo="Nombre" placeholder="Ingresar nombre"/>
+            <Texto titulo="Rango" placeholder="Ingresar rango"/>
+            <Texto titulo="Foto" placeholder="Ingresar foto"/>
         </form>
     </section>
 }
+
+export  default Formulario
