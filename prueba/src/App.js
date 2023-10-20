585c889cf1bbc1d47e0fea9749b3b32a5227fdb1
GbyOtz <anagabrielaortizv16@gmail.com>
2023-10-19 20:10:04 -0400

    modificacion formulario

--- prueba/src/App.js
+++ prueba/src/App.js
@@ -3,14 +3,16 @@ import './componentes/Header/Header.js';
 import './App.css';
 import Header from './componentes/Header/Header.js';
 import Formulario from './componentes/Formulario/Formulario.js';
+import ghost from './componentes/Ghost/index.js';
 
 function App() {
   return ( 
-       
     <div>
       <Header/>
       <Formulario/>
+      <ghost></ghost>
     </div>
+    
   );
 }
 
